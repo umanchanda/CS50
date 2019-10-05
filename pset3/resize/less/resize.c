e3c0c86bc4764b9eb374a3ca3a1bf7e9fbc8daed
Uday Manchanda <uday.manchanda14@gmail.com>
2019-10-05 16:19:19 -0400

    modified resize

--- pset3/resize/less/resize.c
+++ pset3/resize/less/resize.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "bmp.h"
 
 int main(int argc, char* argv[])
 {
@@ -8,5 +9,45 @@ int main(int argc, char* argv[])
         printf("Usage: ./resize n infile outfile");
     }
 
+    int n;
+    do
+    {
+        n = atoi(argv[1]);
+    } while (n < 0 || n > 100);
     
+    char *infile = argv[2];
+    char *outfile = argv[3];
+
+    FILE *inptr = fopen(infile, "r");
+    if (inptr == NULL)
+    {
+        fprintf(stderr, "Could not open %s.\n", infile);
+        return 2;
+    }
+
+    FILE *outptr = fopen(outfile, "w");
+    if (outptr == NULL)
+    {
+        fclose(inptr);
+        fprintf(stderr, "Could not create %s.\n", outfile);
+        return 3;
+    }
+
+    BITMAPFILEHEADER bf;
+    fread(&bf, sizeof(BITMAPFILEHEADER), 1, inptr);
+
+    BITMAPINFOHEADER bi;
+    fread(&bi, sizeof(BITMAPINFOHEADER), 1, inptr);
+
+    // ensure infile is (likely) a 24-bit uncompressed BMP 4.0
+    if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 ||
+        bi.biBitCount != 24 || bi.biCompression != 0)
+    {
+        fclose(outptr);
+        fclose(inptr);
+        fprintf(stderr, "Unsupported file format.\n");
+        return 4;
+    }
+
+
 }
