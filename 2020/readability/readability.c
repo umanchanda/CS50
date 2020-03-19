179ed715cf4792f84baabf4014bd4ce3f9552fba
Uday Manchanda <uday.manchanda14@gmail.com>
2020-03-18 23:02:14 -0400

    edit

---
+++ 2020/readability/readability.c
@@ -0,0 +1,25 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <math.h>
+
+float getLetters(string text)
+{
+    for (int i = 0; i < text.length(); i++)
+    {
+        
+    }
+}
+
+float getSentences(string text)
+{
+    return 0.0;
+}
+
+int main()
+{
+    string text = get_string("Text: ");
+    int grade = round(0.0588 * getLetters(text) - 0.296 * getSentences(text) - 15.8);
+    printf("Grade %d\n", grade);
+}
